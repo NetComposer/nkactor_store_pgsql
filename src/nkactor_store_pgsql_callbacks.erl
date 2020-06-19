9e8bc2d838a5fdaf17a5d4e17eb00a11861e0e2f
Carlos González <carlos@erlnets.com>
2020-06-19 20:05:05 +0200

    New notifications

--- src/nkactor_store_pgsql_callbacks.erl
+++ src/nkactor_store_pgsql_callbacks.erl
@@ -24,7 +24,7 @@
 
 
 -export([actor_store_pgsql_parse/4, actor_store_pgsql_unparse/4]).
--export([status/1]).
+-export([status/1, parse_actors/5, new_span/4]).
 -export([actor_db_init/1,
          actor_db_find/3, actor_db_read/3, actor_db_create/3, actor_db_update/3,
          actor_db_delete/3, actor_db_delete_multi/3, actor_db_search/3, actor_db_aggregate/3,
