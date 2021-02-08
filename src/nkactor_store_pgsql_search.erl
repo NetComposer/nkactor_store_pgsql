23ab55f3bf24f944f2c9abcb45ff13a44e375ba8
Carlos González <carlos@erlnets.com>
2021-02-08 13:08:39 +0100

    Remove test

--- src/nkactor_store_pgsql_search.erl
+++ src/nkactor_store_pgsql_search.erl
@@ -133,8 +133,6 @@ search(actors_search_generic, Params) ->
         <<" OFFSET ">>, to_bin(From), <<" LIMIT ">>, to_bin(Size),
         <<";">>
     ],
-    lager:warning("Q: ~s", [list_to_binary(Query)]),
-
     {query, Query, fun ?MODULE:pgsql_actors/2};
 
 search(actors_search_generic_labels, #{do_count:=true}=Params) ->
