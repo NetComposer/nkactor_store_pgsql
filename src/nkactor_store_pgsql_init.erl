01124bcfe108a77ca57548d80d23e3921fef74ad
Carlos González <carlos@erlnets.com>
2021-02-10 17:14:35 +0100

    New iter index

--- src/nkactor_store_pgsql_init.erl
+++ src/nkactor_store_pgsql_init.erl
@@ -121,7 +121,7 @@ create_database_query(postgresql) ->
             fts_words TEXT
         );
         CREATE UNIQUE INDEX name_idx on actors (namespace, \"group\", resource, name);
-        CREATE INDEX actors_name_idx on actors (\"group\", resource, name);
+        CREATE INDEX actors_iter_idx on actors (\"group\", resource, uid);
 
         --- text_pattern_ops optimizes LIKE '...%'
         CREATE INDEX last_update_idx on actors (last_update DESC, \"group\", resource, name text_pattern_ops);
